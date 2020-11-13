2d857af436316674991695af9a05ebdb999f3fbb
cordenbrock <cordenbrock@gmail.com>
2020-11-13 15:25:04 -0800

    purposefully pass refactored test for spec 1

--- __tests__/human.test.js
+++ __tests__/human.test.js
@@ -9,6 +9,6 @@ describe('Human', () => {
   });
 
 test('Should correctly construct Human object with age and attribute properties', () => {
-    expect(human).toEqual( {"age": 30, "this.attr1": 5, "this.attr2": 15, "this.attr3": 25, "this.lifeExpectancy": 100} );
+    expect(human).toEqual( {"age": 30, "attr1": 5, "attr2": 15, "attr3": 25, "lifeExpectancy": 100} );
   });
 });  
