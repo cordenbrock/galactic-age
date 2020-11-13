288f371db470c19c40099b7fef5101e919470ee6
cordenbrock <cordenbrock@gmail.com>
2020-11-13 15:38:39 -0800

    Purposefully pass test for spec 6

--- __tests__/human.test.js
+++ __tests__/human.test.js
@@ -12,6 +12,6 @@ test('Should correctly construct Human-object with age and attribute properties'
     expect(human).toEqual( {"age": 30, "attr1": 5, "attr2": 15, "attr3": 25, "lifeExpectancy": 100} );
   });
   test('Should correctly calculate Earth-life-expectancy based on user-attributes', () => {
-    expect(human.calcEarthLifeExpectancy()).toEqual(100);
+    expect(human.calcEarthLifeExpectancy()).toEqual(104.5);
   })
 });  
