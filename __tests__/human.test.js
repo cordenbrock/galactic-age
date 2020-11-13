a93c5ac600aa2c69856e5637ffd090aeb6b946e3
cordenbrock <cordenbrock@gmail.com>
2020-11-13 11:50:21 -0800

    Update and purposefully fail test for spec 1

--- __tests__/human.test.js
+++ __tests__/human.test.js
@@ -8,7 +8,8 @@ describe('Human', () => {
     human = new Human(30)
   });
 
-  test('Should correctly construct Human object with an age property', () => {
+  test('Should correctly construct Human object with an age and jacks property', () => {
   expect(human.age).toEqual(30);
+  expect(human.jacks).toEqual();
   });
 });  
