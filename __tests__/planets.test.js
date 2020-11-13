d994728d6df71f199bf813cc618f03ed83d3a4d2
cordenbrock <cordenbrock@gmail.com>
2020-11-13 13:14:14 -0800

    Purposefully pass test for spec 2

--- __tests__/planets.test.js
+++ __tests__/planets.test.js
@@ -11,6 +11,6 @@ describe('Planets', () => {
   expect(planet.earthAge).toEqual(30);
   });
   test('Should correctly calculate earth age on mercury', () => {
-    expect(planet.calcMercuryAge(planet.earthAge)).toEqual(126);
+    expect(planet.calcMercuryAge(planet.earthAge)).toEqual(125);
     });
 });  
