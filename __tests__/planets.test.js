954b9b536b1d6b4087aea30ea934b7064e4296f8
cordenbrock <cordenbrock@gmail.com>
2020-11-13 13:02:25 -0800

    Add test to calculate earth age on mercury

--- __tests__/planets.test.js
+++ __tests__/planets.test.js
@@ -3,7 +3,6 @@ import Planets from './../src/js/planets';
 describe('Planets', () => {
   
   let planet;
-
   beforeEach(() => {
     planet = new Planets(30);
   });
@@ -11,4 +10,7 @@ describe('Planets', () => {
   test('Should correctly instantiate Planet object', () => {
   expect(planet.earthAge).toEqual(30);
   });
+  test('Should correctly calculate earth age on mercury', () => {
+    expect(calcMercuryAge(planet.earthAge)).toEqual();
+    });
 });  
