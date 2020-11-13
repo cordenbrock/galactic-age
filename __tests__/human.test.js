71d1a0690eb2fb66e15a644e28bf6d10e103a8d6
cordenbrock <cordenbrock@gmail.com>
2020-11-13 15:09:45 -0800

    Purposefully pass test for spec 1

--- __tests__/human.test.js
+++ __tests__/human.test.js
@@ -12,6 +12,6 @@ test('Should correctly construct Human object with age and attribute properties'
     expect(human.age).toEqual(30);
     expect(human.attr1).toEqual(5);
     expect(human.attr2).toEqual(15);
-    expect(human.attr3).toEqual(0);
+    expect(human.attr3).toEqual(25);
   });
 });  
