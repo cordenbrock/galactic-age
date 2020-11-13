946ebd39bbbe1e8fb99a1a577fa9c9bb9a8a8e99
cordenbrock <cordenbrock@gmail.com>
2020-11-13 10:30:40 -0800

    Write test suite for human constructor with age property

---
+++ __tests__/human.test.js
@@ -0,0 +1,14 @@
+import Human from './../src/js/human.js'
+
+describe('Human', () => {
+  
+  let human;
+
+  beforeEach(() => {
+    human = new Human(30)
+  });
+
+  test('Should correctly construct Human object with an age property', () => {
+  expect(age).toEqual(3);
+  });
+});  
