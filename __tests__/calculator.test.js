c84f6e85d406fa53c8b8e82e825c68849402ccbc
cordenbrock <cordenbrock@gmail.com>
2020-11-15 22:07:46 -0800

    Fix test typo, fail test, and pass test

--- __tests__/calculator.test.js
+++ __tests__/calculator.test.js
@@ -6,6 +6,6 @@ test('Should correctly calculate solar ages', () => {
     expect(new CalcSolarAges(30)).toEqual( {"earthAge": 30, "jupiterAge": 3, "marsAge": 16, "mercuryAge": 125, "venusAge": 48} );
   });
   test('Should correctly calculate solar life expectancies to nearest integer', () => {
-    expect(new CalcSolarLifeExpectancies(105)).toEqual( {"earthAge": 104, "jupiterAge": 8, "marsAge": 55, "mercuryAge": 437, "venusAge": 168} );
+    expect(new CalcSolarLifeExpectancies(105)).toEqual( {"earthExpectancy": 105, "mercuryExpectancy": 438, "venusExpectancy": 169, "marsExpectancy": 56, "jupiterExpectancy": 9} );
   });
-});  
+});
