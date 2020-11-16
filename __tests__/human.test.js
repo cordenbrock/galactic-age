da58069aa125304497ef1e5dd272309547d37595
cordenbrock <cordenbrock@gmail.com>
2020-11-15 23:20:52 -0800

    Add second expectation for full test coverage, purposefully fail test

--- __tests__/human.test.js
+++ __tests__/human.test.js
@@ -15,6 +15,8 @@ test('Should correctly calculate Earth-life-expectancy based on user-attributes'
   });
 test('Should correctly calculate how many solar life years that user either has remaining or has surpassed in expectancy on respective planet', () => {
     let expectancyAge = human.calcAdjustedLifeExpectancy();
-    expect(human.compareAgeAndLifeExpectencies(human.age, expectancyAge)).toEqual(74);
+    expect(human.compareAgeAndLifeExpectencies(human.age, expectancyAge)).toEqual(75);
+    let currentAge = 110;
+    expect(human.compareAgeAndLifeExpectencies(currentAge, expectancyAge)).toEqual(0);
   });
 });  
