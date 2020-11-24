daece40c7b0553fecb08e2b3c58b329df024afe5
cordenbrock <cordenbrock@gmail.com>
2020-11-23 16:49:30 -0800

    Refactor tests, purposefully fail

--- __tests__/human.test.js
+++ __tests__/human.test.js
@@ -2,21 +2,23 @@ import Human from './../src/js/human.js'
 
 describe('Human', () => {
   
-  let human;
+  let human, surpassedExpectancyHuman;
   beforeEach(() => {
-    human = new Human(30, 5, 15, 25)
+    human = new Human(30, 5, 15, 25);
+    human.calcAdjustedLifeExpectancy();
+    surpassedExpectancyHuman = new Human(110, 0, 0, 0)
+    surpassedExpectancyHuman.calcAdjustedLifeExpectancy();
   });
 
-test('Should correctly construct Human-object with age and attribute properties', () => {
-    expect(human).toEqual( {"age": 30, "attr1": 5, "attr2": 15, "attr3": 25, "lifeExpectancy": 100} );
+  test('Should correctly calculate Earth-life-expectancy based on user-attributes', () => {
+    expect(human.adjustedLifeExpectancy).toEqual(105);
   });
-test('Should correctly calculate Earth-life-expectancy based on user-attributes', () => {
-    expect(human.calcAdjustedLifeExpectancy()).toEqual(105);
+  test('Should correctly calculate how many solar life years that the user has remaining on a planet', () => {
+    human.compareAgeAndLifeExpectancies(human.age, human.adjustedLifeExpectancy);
+    expect(human.yearsLeft).toEqual(50);
   });
-test('Should correctly calculate how many solar life years that user either has remaining or has surpassed in expectancy on respective planet', () => {
-    let expectancyAge = human.calcAdjustedLifeExpectancy();
-    expect(human.compareAgeAndLifeExpectancies(human.age, expectancyAge)).toEqual(75);
-    let currentAge = 110;
-    expect(human.compareAgeAndLifeExpectancies(currentAge, expectancyAge)).toEqual(5);
+  test('Should correctly calculate how many solar life years that the user has surpassed in expectancy on a planet', () => {
+    surpassedExpectancyHuman.compareAgeAndLifeExpectancies(surpassedExpectancyHuman.age, surpassedExpectancyHuman.adjustedLifeExpectancy);
+    expect(surpassedExpectancyHuman.yearsSurpassed).toEqual(25);
   });
 });  
